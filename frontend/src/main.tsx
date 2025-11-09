d14ab81e3a220af8f57c2246f524970b98ddc131
Gitdono16 <104618933+Gitdono16@users.noreply.github.com>
2025-11-09 11:07:01 +0100

    github_page3

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+
+const redirect = sessionStorage.redirect;
+if (redirect) {
+    delete sessionStorage.redirect;
+    window.history.replaceState(null, "", redirect);
+}
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <App />
